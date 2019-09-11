19a2bf21ee94fe599f6d7804c9bbeb8f29343aac
jstam <jstam009@gmail.com>
2019-09-10 21:07:06 -0400

    Add project files.

---
+++ Week3Assignment2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Week3Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years experience do you have in professional programming?");
+
+            string input = Console.ReadLine();
+            int value_of_input = int.Parse(input);
+            
+            if(value_of_input >= 0)
+            {
+                for (int i = 0; i <= value_of_input; i++)
+                {
+                    Console.WriteLine("You have " + i.ToString() + " years of experience.");
+                }
+            }
+        }
+    }
+}
